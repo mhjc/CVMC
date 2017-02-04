de748e1bab68dde7db2b429609386d0b7a099691
JCMH\JC <medina.jc03@gmail.com>
2017-02-04 17:12:19 -0600

    2 Carpetas 1. respaldo 2. pruebas

---
+++ CirculoRespaldo/index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<title>Home</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+	<link href="https//fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link rel="stylesheet" href="flexslider.css" type="text/css">
+	<link rel="stylesheet" href="css/estilos.css">
+	<link rel="stylesheet" href="css/menu.css">
+	<link rel="stylesheet" href="css/banner.css">
+	<link rel="stylesheet" href="css/blog.css">
+	<link rel="stylesheet" href="css/info.css">
+	<link rel="stylesheet" href="css/flexslider.css">
+</head>
+<body>
+	
+	<header>
+	    <div class="contenedor">
+            <h1>Círculo Virtuoso</h1>
+	        <input type="checkbox" id="menu-bar">
+	        <label class="fa fa-bars" id="etiqueta" for="menu-bar"></label>
+	        <nav class="menu">
+                <li><a href="#">Comercio <i class="icono derecha fa fa-chevron-down"></i></a>
+                    <ul>
+                        <li><a href="https://www.google.com.mx">Carnicería</a></li>
+                        <li><a href="#">Pollería</a></li>
+                        <li><a href="#">Panadería</a></li>
+                        <li><a href="#">Farmacia</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Servicio <i class="icono derecha fa fa-chevron-down"></i></a>
+                    <ul>
+                        <li><a href="#">Service 1</a></li>
+                        <li><a href="#">Service 2</a></li>
+                        <li><a href="#">Service 3</a></li>
+                        <li><a href="#">Service 4</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Industria <i class="icono derecha fa fa-chevron-down"></i></a>
+                    <ul>
+                        <li><a href="#">Herrería</a></li>
+                        <li><a href="#">Carpintería</a></li>
+                        <li><a href="#">Impresión</a></li>
+                        <li><a href="#">Artesanal</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Infraestructura <i class="icono derecha fa fa-chevron-down"></i></a>
+                    <ul>
+                        <li><a href="#">Infra 1</a></li>
+                        <li><a href="#">Infra 2</a></li>
+                        <li><a href="#">Infra 3</a></li>
+                        <li><a href="#">Infra 4</a></li>
+                        <li><a href="#">Infra 5</a></li>
+                        <li><a href="#">Infra 6</a></li>
+                    </ul>
+                </li>
+	        </nav>
+	    </div>
+	</header>
+	
+	<main>
+	    <section id="banner">
+            <img src="img/construccion.jpg">
+            <div class="contenedor">
+            </div>
+	    </section>
+	    
+	    <!-- 
+        <section id="bienvenidos">
+	        <h2>Círculo Virtuoso Magdalena Contreras</h2>
+	        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam quaerat aut enim!</p>
+	    </section>
+	    -->
+	<footer>
+	    <div class="contenedor">
+	        <p class="copy">Circle &copy 2017</p>
+	        <div class="sociales">
+	            <a class="fa fa-facebook-official" href=""></a>
+	            <a class="fa fa-youtube" href=""></a>
+	            <a class="fa fa-twitter" href=""></a>
+	            <a class="fa fa-instagram" href=""></a>
+	        </div>
+	    </div>
+	</footer>
+
+	<script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+	<script src="js/menu.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+    <script src="js/slider.js"></script>
+    <script src="js/jquery.flexslider.js"></script>
+</body>
+</html>
